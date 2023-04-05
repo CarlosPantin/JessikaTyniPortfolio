ef6cad7e134ac2e87c7e07395b3f9f4ffd44b11b
CarlosPantin~ <carlospantin@live.com.mx>
2023-04-05 14:19:14 +0300

    Changes to be committed: modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a href=""><FiArrowRight /></a>
-              <a href="https://linkedin.com/in/carlospantinp"><AiFillLinkedin /></a>
+              <a href="https://www.linkedin.com/in/jessika-tyni-806222220/"><AiFillLinkedin /></a>
               <a href=""><FiArrowLeft /></a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full  relative overflow-hidden mt-20  sm:h-20 sm:w-20 md:h-96 md:w-96">
