d74501522380682228599a5076231c1504b848aa
CarlosPantin~ <carlospantin@live.com.mx>
2023-05-30 10:23:09 +0300

    Changes to be committed: modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">Jessika Tyni</h1>
+            <h1 className="font-burtons text-xl">Jessika Tyni LOBSTER :D</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
